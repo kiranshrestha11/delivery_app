0803fd5f3f5e30363186b71eef455fcefda3a631
kiran Shrestha <kiranstha1679@gmail.com>
2022-05-22 12:30:19 +0545

    project started

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:delivery_app/views/login/login.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Delivery',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Login(),
+    );
+  }
+}
